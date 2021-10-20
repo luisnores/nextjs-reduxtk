6e6110568b36b63a9b17794f8cdea079a52ad931
luisnores <Luis.Nores@corcoran.com>
2021-10-20 12:15:02 -0500

    Adding RTK, axios to handle sync and async code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '@styles/globals.css';
+import '@styles/app.css';
+import { Provider } from 'react-redux';
+
+import store from '@store/store';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
